86894c11df44aa76bf4f78b272576bba1743a28f
DerFussek <tildussel@outlook.de>
2025-03-24 19:20:28 +0100

    Update Servo.ino

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -8,10 +8,15 @@ void setup() {
   Serial.begin(9600);
   servo.attach(ServoPin);
   servo.write(0);
+
 }
 
 void loop() {
+  
+  Serial.println("Bitte Wert von 0 - 180 eingeben: ");
+  while(Serial.available() == 0) {};
   servo.write(getInt());
+
 }
 
 int getInt() {
@@ -20,6 +25,10 @@ int getInt() {
   String input = Serial.readStringUntil('\n');
   int value = input.toInt();
   
-  if(value == 0 && input != "0") return;
+  if(value == 0 && input != "0") {
+    Serial.println("Gueltigen Wert eingeben");
+    return;
+  }
+  
   return value;
 }
