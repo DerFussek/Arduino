15fd4a7e9e82891b231a2e94d4af25d72017afb4
DerFussek <tildussel@outlook.de>
2025-05-02 20:33:02 +0200

    Update tftUhr.ino

--- Privat/tftUhr/tftUhr.ino
+++ Privat/tftUhr/tftUhr.ino
@@ -5,18 +5,35 @@
 #include <Wire.h>
 #include <DS3231.h>
 
-// Display-Pins
+//***************Tft-Display***************//
 #define TFT_CS    25
 #define TFT_RST   23
 #define TFT_DC    27
 #define TFT_MOSI  29
 #define TFT_SCLK  31
 
-// Display- und RTC-Objekte
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+//***************RealTimeClock***************//
 DS3231 myRTC;
+
 bool h12, PM, century;
 String wochentag[] {"So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"};
+
+
+void setup(void) {
+  Wire.begin();
+  Serial.begin(9600);
+
+  tft.initR(INITR_GREENTAB);
+
+  drawStaticUserInterface();
+ 
+}
+
+void loop() {
+  updateDynamicUserInterface(1000);
+}
 
 // Kreis-Zeichenfunktion
 void drawArc(int x, int y, int r, int startAngle, int endAngle, int thickness, uint16_t color) {
@@ -30,105 +47,136 @@ void drawArc(int x, int y, int r, int startAngle, int endAngle, int thickness, u
   }
 }
 
-void setup(void) {
-  Wire.begin();
-  Serial.begin(9600);
-
-  tft.initR(INITR_GREENTAB);
+//Zeichnet statische Anzeige beim Start
+void drawStaticUserInterface() {
+  tft.fillScreen(ST77XX_BLACK); //Displayinhalt löschen
   
-  tft.fillScreen(ST77XX_BLACK);
-  delay(500);
-  // Statische Anzeige beim Start
+
   tft.setTextColor(tft.color565(0, 255, 165));
   tft.setTextSize(4);
 
-  // Uhrzeit berechnen & anzeigen
-  byte hour = myRTC.getHour(h12, PM);
+  //Uhrzeit berechnen & anzeigen
+  byte hour = myRTC.getHour(h12, PM); //aktuelle Uhzeit hohlen
   byte minute = myRTC.getMinute();
-  String uhrTxt = (hour < 10 ? "0" : "") + String(hour) + ":" +
+  String uhrTxt = (hour < 10 ? "0" : "") + String(hour) + ":" +   //aktuelle uhrzeit als String speichern
                   (minute < 10 ? "0" : "") + String(minute);
-  const char* uhrTxtC = uhrTxt.c_str();
+  
+  const char* uhrTxtC = uhrTxt.c_str(); //Arduino-String wird in C-String umgewandelt
 
-  int16_t _uhrX, _uhrY;
-  uint16_t uhrW, uhrH;
-  tft.getTextBounds(uhrTxtC, 0, 0, &_uhrX, &_uhrY, &uhrW, &uhrH);
-  int16_t uhrX = (128 - uhrW) / 2 - _uhrX + 3;
-  int16_t uhrY = (160 - uhrH) / 2 - _uhrY + 26;
-  tft.setCursor(uhrX, uhrY);
-  tft.print(uhrTxtC);
+  int16_t _uhrX, _uhrY; //soll x + y Position des textes speichern
+  uint16_t uhrW, uhrH;  //soll breite und höhe des textes
+  
+  tft.getTextBounds(uhrTxtC, 0, 0, &_uhrX, &_uhrY, &uhrW, &uhrH); //hier wird die breite, höhe und x + y position 
+                                                                  //der linkeren oberen ecke des Tetxes in verschiedenen Variablen gespeichert
+  
+  int16_t uhrX = (128 - uhrW) / 2 - _uhrX + 3;    //x Position ausrechen, an dem der Text zentriert ist
+  int16_t uhrY = (160 - uhrH) / 2 - _uhrY + 26;   //y Position ausrechen, an dem der Text zentriert ist und von der Mitte um 26 pixel nach unten verschieben
+  
+  tft.setCursor(uhrX, uhrY);  //Uhrzeit auf Display zeichnen
+  tft.print(uhrTxtC);         //
+
+
 
   // Datum anzeigen
-  byte day = myRTC.getDate();
-  byte month = myRTC.getMonth(century);
-  byte year = myRTC.getYear();
-  String dateTxt = (day < 10 ? "0" : "") + String(day) + "." +
-                   (month < 10 ? "0" : "") + String(month) + "." +
-                   "20" + String(year);
-  const char* dateTxtC = dateTxt.c_str();
+  byte day = myRTC.getDate();           //Aktueller Tag, Monat und Jahr in Variablen speichern
+  byte month = myRTC.getMonth(century); // 
+  byte year = myRTC.getYear();          //
+  String dateTxt = (day < 10 ? "0" : "") + String(day) + "." +                          //
+                   (month < 10 ? "0" : "") + String(month) + "." + "20" + String(year); //Datum zusammengesetzt als String speichern
+  
+  const char* dateTxtC = dateTxt.c_str();   //Datum als C-String speichern                                         
 
-  tft.setTextColor(tft.color565(255, 255, 255));
-  tft.setTextSize(1);
+  tft.setTextColor(tft.color565(255, 255, 255));  //Textfarbe auf Weiß setzen
+  tft.setTextSize(1); //Größe des textes festlegen
 
-  int16_t _dateX, _dateY;
-  uint16_t dateW, dateH;
-  tft.getTextBounds(dateTxtC, 0, 0, &_dateX, &_dateY, &dateW, &dateH);
-  int16_t dateX = (128 - dateW) / 2 - _dateX;
-  int16_t dateY = uhrY + uhrH + 5;
-  tft.setCursor(dateX, dateY);
-  tft.print(dateTxtC);
+  int16_t _dateX, _dateY; //soll x + y Position des textes speichern
+  uint16_t dateW, dateH;  //soll breite und höhe des textes
+  
+  tft.getTextBounds(dateTxtC, 0, 0, &_dateX, &_dateY, &dateW, &dateH); //hier wird die breite, höhe und x + y position 
+                                                                       //der linkeren oberen ecke des Tetxes in verschiedenen Variablen gespeichert
+  
+  int16_t dateX = (128 - dateW) / 2 - _dateX; //Mitte herausfinden
+  int16_t dateY = uhrY + uhrH + 5;  //Position 5px unter der Uhrzeit 
+  
+  tft.setCursor(dateX, dateY);  //Datum auf Display zeichen
+  tft.print(dateTxtC);          //
+
+
 
   //Wochentag
-  tft.setTextColor(tft.color565(255, 255, 255));
-  tft.setTextSize(1);
+  tft.setTextColor(tft.color565(255, 255, 255));  //Textfarbe festlegen
+  tft.setTextSize(1);                             //Textgröße festlegen
 
-  String DowTxt = wochentag[myRTC.getDoW()];
+  String DowTxt = wochentag[myRTC.getDoW()]; //aktuellen Wochentag abfragen und dessen abkürzung in einem String speichern
+
   int16_t _DowX, _DowY;
   uint16_t DowW, DowH;
+
   const char* DowTxtC = DowTxt.c_str();
+
   tft.getTextBounds(DowTxtC, 0, 0, &_DowX, &_DowY, &DowW, &DowH);
+  
   int16_t DowX = (128 - DowW) / 2 - _DowX;
   int16_t DowY = uhrY - 15;
-  tft.setCursor(DowX, DowY);
-  tft.print(DowTxtC);
+  
+  tft.setCursor(DowX, DowY);  //
+  tft.print(DowTxtC);         //Wochentag auf dem Display anzeigen
 
+  
+
+  //**Kreise**/
   tft.setTextSize(2);
-  // Kreise
   int16_t circleR = 20;
-  // Links
-  tft.fillCircle(32, 38, circleR, tft.color565(3, 26, 12));
-  tft.fillCircle(32, 38, circleR - 3, tft.color565(0, 0, 0));
-  drawArc(32, 38, circleR, 0, 180, 3, tft.color565(0, 255, 165));
-  tft.setCursor(27, 32);
-  tft.println("3");
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(1);
-  tft.setCursor(16, 62);
-  tft.print("Stufe");
+  
+  //Stufe
+  tft.fillCircle(32, 38, circleR, tft.color565(3, 26, 12));   //dunkelgrüner Kreis mit einem Radius von 20px zeichen
+  tft.fillCircle(32, 38, circleR - 3, tft.color565(0, 0, 0)); //Schwarzen Kreis mit einem Radius von 17px an der gleichen stelle zeichnen,
+                                                              //um einen Ring von 3px zu zeichenn
+  
+  drawArc(32, 38, circleR, 0, 180, 3, tft.color565(0, 255, 165)); //Laufring zeichnen
+  
+  tft.setCursor(27, 32);  //Text mittig im Kreis ausrichten
+  tft.println("3"); //Stufe zeichen
+
+  tft.setTextColor(ST77XX_WHITE); //Text "Stufe" unter dem Kreis zeichen
+  tft.setTextSize(1);             //
+  tft.setCursor(16, 62);          //
+  tft.print("Stufe");             //
 
   // Rechts
-  tft.fillCircle(96, 38, circleR, tft.color565(0, 25, 50));
-  tft.fillCircle(96, 38, circleR - 3, tft.color565(0, 0, 0));
-  drawArc(96, 38, circleR, 0, 250, 3, tft.color565(0, 128, 255));
-  tft.setCursor(86, 32);
-  tft.println("00");
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(1);
-  tft.setCursor(84, 62);
-  tft.print("Temp.");
+  tft.fillCircle(96, 38, circleR, tft.color565(0, 25, 50));   //dunkelorangen Kreis mit einem Radius von 20px zeichen
+  tft.fillCircle(96, 38, circleR - 3, tft.color565(0, 0, 0)); //Schwarzen Kreis mit einem Radius von 17px an der gleichen stelle zeichnen,
+                                                              //um einen Ring von 3px zu zeichenn
+  
+  drawArc(96, 38, circleR, 0, 250, 3, tft.color565(0, 128, 255)); //Laufring zeichnen
+  
+  tft.setCursor(86, 32); //Text mittig im Kreis ausrichten
+  tft.println("00"); //Platzhalter für die Temperatur zeichnen (später Variable)
+  
+  tft.setTextColor(ST77XX_WHITE); //Text "Temp." unter dem Kreis zeichen
+  tft.setTextSize(1);             //
+  tft.setCursor(84, 62);          //
+  tft.print("Temp.");             //
 }
 
-void loop() {
+void updateDynamicUserInterface(int delayMs) {
   static String lastTime = "";
   static byte lastTemp = "";
+  static int32_t lastDelay = millis();
 
-  byte hour = myRTC.getHour(h12, PM);
-  byte minute = myRTC.getMinute();
+  if(!(millis() - lastDelay) >= delayMs) return;
+
+  byte hour = myRTC.getHour(h12, PM);   //Aktuelle Stunde abfragen
+  byte minute = myRTC.getMinute();  //Aktuelle Minute abfragen
+  byte temp = myRTC.getTemperature(); //Aktuelle Temperatur abfragen
+
   String uhrTxt = (hour < 10 ? "0" : "") + String(hour) + ":" +
                   (minute < 10 ? "0" : "") + String(minute);
-  byte temp = myRTC.getTemperature();
 
-  if (uhrTxt != lastTime) {
+  if(uhrTxt != lastTime) {
     lastTime = uhrTxt;
+
+    
 
     // Hintergrundfläche für Uhrzeit löschen
     tft.fillRect(0, 85, 128, 35, ST77XX_BLACK);
@@ -162,5 +210,5 @@ void loop() {
     tft.println(temp);
   }
 
-  delay(200);
+  lastDelay = millis();
 }
